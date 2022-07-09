60fc22742fea4a4e16d6b333567a2d99c187cdd5
h1n3s1ght <benjaminisaachines@gmail.com>
2022-07-09 12:08:40 -0500

    Added templates

--- snowball_project/settings.py
+++ snowball_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_%764&2ufi9c&_ykf!6%^7atx6lmby06)(!b7)nv0(rou=^d_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
